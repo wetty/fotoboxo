c5b33196a5609fcb53ff2ace11ddcee26400a9d6
Thomas Wetterer <thomas@wetterer.de>
2016-01-20 20:58:56 +0100

    tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -48,6 +48,24 @@ module.exports = function(grunt) {
         }]
       }
     },
+	uncss: {
+	  dist: {
+	    files: {
+	      'assets/css/tidy.css': ['_site/index.html', '_site/news/index.html', '_site/fotobox/index.html']
+	    }
+	  }
+	},
+	cssmin: {
+	  target: {
+	    files: [{
+	      expand: true,
+	      cwd: 'assets/css',
+	      src: ['*.css', '!*.min.css'],
+	      dest: 'assets/css',
+	      ext: '.min.css'
+	    }]
+	  }
+	},
     watch: {
       js: {
         files: [
@@ -70,13 +88,17 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-svgmin');
+  grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
 //  grunt.loadNpmTasks('grunt-responsive-images');
 
   // Register tasks
   grunt.registerTask('default', [
     'clean',
     'uglify',
-    'imagemin'
+    'imagemin',
+    'uncss',
+	'cssmin'
   ]);
   grunt.registerTask('dev', [
     'watch'
