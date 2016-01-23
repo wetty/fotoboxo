e96c6e1ce57899dbaa180adcc5acaac19b7e6d00
Thomas Wetterer <thomas@wetterer.de>
2016-01-23 18:12:56 +0100

    load fonts async

---
+++ assets/js/plugins/xx_load_fonts.js
@@ -0,0 +1,5 @@
+WebFont.load({
+  google: {
+    families: ['Open Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800','Merriweather:400,300,300italic,400italic,700,700italic,900,900italic']
+  }
+});
